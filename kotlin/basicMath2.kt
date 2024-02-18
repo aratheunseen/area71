7e8705cf3863b1da0b68afece8aee355230d66a0
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-02-18 21:01:23 +0600

    Refactor code for improved performance and readability

---
+++ kotlin/basicMath2.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+    
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = add(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
+}
+
+fun add(first:Int, second:Int) : Int {
+    return first + second;
+}
