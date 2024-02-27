9b9407e2cd0da98dc330a22999f19899206e77a9
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-02-27 14:29:45 +0600

    Add CLI functionality to store and display todos

---
+++ python/cli-app/main.py
@@ -0,0 +1,14 @@
+while True:
+    prompt = input("Enter your todo: ")
+
+    todo = open("todo.txt", "a")
+    todo.write(prompt + "\n")
+    todo.close()
+
+    if prompt == "exit":
+        break
+
+print("Todo added successfully!")
+print("Your todo list is: ")
+todo = open("todo.txt", "r")
+print(todo.read())
