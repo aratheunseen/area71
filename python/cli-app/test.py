c122ecad93ab819db99ed83e4b782c894f1f5d56
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-02-28 15:22:25 +0600

    Refactor temperature conversion to use integers

--- python/cli-app/test.py
+++ python/cli-app/test.py
@@ -1,4 +1,3 @@
-temperatures = [10, 12, 14]
-file = open("file.txt", 'w')
-temperatures = [str(temp) + "\n" for temp in temperatures]
-file.writelines(temperatures)
+numbers = [10.1, 12.3, 14.7]
+numbers = [int(number) for number in numbers]
+print(numbers)
