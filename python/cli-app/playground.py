410f73320ad0e7ef9ecda5a1c70aacf7549d42f8
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-02 01:44:13 +0600

    Refactor code to import functions from separate module

--- python/cli-app/playground.py
+++ python/cli-app/playground.py
@@ -1,13 +1,4 @@
-def get_todos(filepath):
-    """ Read a text file and return the list of todos from the file. """
-    with open(filepath, 'r') as get_file:
-        todos_loc = get_file.readlines()
-    return todos_loc
-
-def set_todos(filepath,todos):
-    """ Write the todo items list int the text file. """
-    with open(filepath, 'w') as set_file:
-        set_file.writelines(todos)
+from functions import get_todos, set_todos
 
 while True:
     user_action = input("YourNote > ")
