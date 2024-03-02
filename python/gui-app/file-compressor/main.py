16e6850c30208f22dc0fd1c06d922ae7625d5365
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-03 05:01:15 +0600

    Refactor GUI layout and add file compression functionality

--- python/gui-app/file-compressor/main.py
+++ python/gui-app/file-compressor/main.py
@@ -1,18 +1,39 @@
 import PySimpleGUI as sg
+import ZipMaker
 
-label1 = sg.Text('Select Files to Compress:')
-input1 = sg.InputText()
-button1 = sg.FilesBrowse('Browse')
-label2 = sg.Text('Select Destination Folder:')
-input2 = sg.InputText()
-button2 = sg.FolderBrowse('Browse')
+select_file_label = sg.Text('Select Files to Compress:')
+select_filepaths = sg.InputText(key='files')
+files_browse_button = sg.FilesBrowse('Choose')
+
+dest_folder_label = sg.Text('Select Destination Folder:')
+destination_folderpath = sg.InputText(key="destination")
+folder_browse_button = sg.FolderBrowse('Choose')
+
+output_label = sg.Text(key='output', size=(50,1))
+
+compress_button = sg.Button('Compress', size=(55,2), button_color=('white', 'green'), key='Compress')
+exit_button = sg.Button('Exit', size=(10,2), button_color=('white', 'red'), key='Exit')
 
 layout = [
-    [label1, input1, button1],
-    [label2, input2, button2],
-    [sg.Button('Compress', size=(55,2), button_color=('white', 'green')), sg.Button('Exit',size=(10,2), button_color=('white', 'red'))]
+    [select_file_label, select_filepaths, files_browse_button],
+    [dest_folder_label, destination_folderpath, folder_browse_button],
+    [compress_button, exit_button],
+    [output_label]
 ]
 
-window = sg.Window('File Compressor', layout)
-window.read()
+window = sg.Window('Files Compressor', layout)
+
+while True:
+    event, values = window.read()
+    print(event, values)
+
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+
+    elif event == 'Compress':
+        filepaths = values['files'].split(';')
+        destination = values['destination']
+        ZipMaker.make_zipfile(filepaths, destination)
+        output_label.update('Files Compressed!')
+
 window.close()
