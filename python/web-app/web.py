263416faf78b2d20bc757df3aa42842875bc79c4
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-05 01:17:08 +0600

    Refactor web.py and todos.txt

--- python/web-app/web.py
+++ python/web-app/web.py
@@ -1,17 +1,26 @@
 import streamlit as st
 from controllers import get_todos, set_todos
 
-todos = get_todos('todos.txt')
+FILEPATH = 'todos.txt'
 
 st.title('YourNote')
 st.write('Welcome to YourNote, a simple note taking app.')
 
-for i, todo in enumerate(todos):
-    st.checkbox(todo, key=i)
+todos = get_todos(FILEPATH)
 
+def add_todo():
+    new_todo = st.session_state['new_todo']
+    todos.append(new_todo+'\n')
+    set_todos(FILEPATH,todos)
+    st.session_state['new_todo'] = ''
 
-new_todo = st.text_input("", placeholder="Add a new todo here...")
-# todos.append(new_todo + '\n')
-# set_todos('todos.txt', todos)
+def delete_todo():
+    for todo in todos:
+        if st.session_state[todo]:
+            todos.remove(todo)
+            set_todos(FILEPATH,todos)
 
-print(new_todo)
+for id,todo in enumerate(todos):
+    st.checkbox(todo,on_change=delete_todo,key=todo)
+
+st.text_input("",placeholder="Add a todo",on_change=add_todo,key='new_todo')
