7004cc6642916bf5a7a51bb7807d174129251a66
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-06 05:24:48 +0600

    Add lined notebook feature and update output file name

--- python/01_intermediates/pdf-maker-app/main.py
+++ python/01_intermediates/pdf-maker-app/main.py
@@ -13,6 +13,12 @@ for index, row in df.iterrows():
     pdf.set_fill_color(100,100,100)
     pdf.line(10,21,200,21)
 
+    # For lined notebook
+    pdf.set_fill_color(100,100,100)
+    for line_height in range (0,245,10):
+        pdf.line(10,21+line_height,200,21+line_height)
+
+    #  For footer text 
     pdf.ln(244)
     pdf.set_font(family="Times",style="I",size=8)
     pdf.set_text_color(180,180,180)
@@ -21,9 +27,17 @@ for index, row in df.iterrows():
     for page in range(row['Pages']-1):
         pdf.add_page()
 
+        # For lined notebook
+        pdf.set_fill_color(100,100,100)
+        for line_height in range (0,245,10):
+            pdf.line(10,21+line_height,200,21+line_height)
+
+        #  For footer text
         pdf.ln(256)
         pdf.set_font(family="Times",style="I",size=8)
         pdf.set_text_color(180,180,180)
         pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
     
-pdf.output("a.pdf")
+# pdf.output("notebook.pdf")
+
+pdf.output("notebook_line.pdf")
