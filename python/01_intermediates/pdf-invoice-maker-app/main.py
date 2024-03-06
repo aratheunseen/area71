77cba3a830eacde650a5cbad5a75b7e331669e51
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-06 06:05:34 +0600

    Add PDF generation functionality

--- python/01_intermediates/pdf-invoice-maker-app/main.py
+++ python/01_intermediates/pdf-invoice-maker-app/main.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("datas/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath,sheet_name="Sheet 1")
-    print(df)
+
+    filename = Path(filepath).stem
+
+    pdf = FPDF(orientation="P",unit="mm",format="A4")
+    pdf.add_page()
+    pdf.set_font(family="Times",style="B",size=16)
+    pdf.cell(w=0,h=8,txt="Invoice ID. ")
 
