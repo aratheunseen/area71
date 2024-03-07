9676a05b346f180a480afb870bf7e9ad8bd518f8
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-07 18:26:08 +0600

    Update main.py to send newsletter with articles from NewsAPI.org

---
+++ python/02_api/newsletter-app-with-api/mailer.py
@@ -0,0 +1,19 @@
+import smtplib,ssl
+
+def send_mail(message):
+
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "admin@gmail.com"
+
+    # Use your own smtp password
+    password = "elaxwpzwphybrblu"
+
+    receiver_mail = "member@gmail.com"
+    context = ssl.create_default_context()
+
+    server = smtplib.SMTP_SSL(host=host, port=port, context=context)
+    server.login(username,password)
+    server.sendmail(username,receiver_mail,message)
+        
--- python/02_api/newsletter-app-with-api/main.py
+++ python/02_api/newsletter-app-with-api/main.py
@@ -1,6 +1,7 @@
 import requests
-import json
+import mailer
 
+# Change NewsAPI.org key (Its a sample key)
 api_key = "06f80f04869d4d3db91252eb5c4b319c"
 url = f"https://newsapi.org/v2/everything?q=apple&from=2024-03-05&to=2024-03-05&sortBy=popularity&apiKey={api_key}"
 
@@ -11,6 +12,17 @@ request = requests.get(url)
 content = request.json()
 
 # Access content
+name = "The Newsletter"
+message = ""
 for article in content['articles']:
-    print(article['title'])
-    print(article)
+    if article['title'] is not None:
+        message = message + article["title"] + "\n" + article["description"] + "\n"
+
+message = message.encode("utf-8")
+
+body = f"""\
+Subject: Hello from {name}
+
+{message}
+"""
+mailer.send_mail(body)
