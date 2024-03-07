b23c50fabf2fa9f1d4c0e2053b726aab4175c6d3
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-08 05:22:20 +0600

    Fix bug in login functionality

---
+++ python/02_api/weather-api/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import pandas
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def weather_report(station, date):
+    # df = pandas.read_csv("")
+    temperature = 23
+    result = {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
+    return result
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
