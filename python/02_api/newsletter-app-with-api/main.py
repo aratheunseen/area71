25e418c4c9184ec0d82590c1933d5f346134348a
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-08 01:40:53 +0600

    Update news API query parameters

--- python/02_api/newsletter-app-with-api/main.py
+++ python/02_api/newsletter-app-with-api/main.py
@@ -1,9 +1,16 @@
 import requests
 import mailer
+from datetime import datetime,date,timedelta
+
+topic = "apple"
+start_date = str((date.today()-timedelta(days=3)).strftime("%Y-%m-%d"))
+end_date = str(datetime.now().strftime("%Y-%m-%d"))
+language = "en"
 
 # Change NewsAPI.org key (Its a sample key)
 api_key = "06f80f04869d4d3db91252eb5c4b319c"
-url = f"https://newsapi.org/v2/everything?q=apple&from=2024-03-05&to=2024-03-05&language=en&sortBy=popularity&apiKey={api_key}"
+url = f"https://newsapi.org/v2/everything?q={topic}&from={start_date}&to={end_date}&language={language}&sortBy=popularity&apiKey={api_key}"
+print(url)
 
 # Make request
 request = requests.get(url)
@@ -14,16 +21,14 @@ content = request.json()
 # Access content
 name = "The Newsletter"
 message = ""
-for article in content["articles"][:10]:
+for article in content["articles"][:20]:
     if article['title'] is not None:
         message = f"{message}\n{article['title']}\n{article['description']}\n{article['url']}\n\n"
 
-mail_body = f"""Subject: Latest update from {name}"\
+mail_body = f"""Subject: Latest update of {topic} from {name}"\
 
 {message}
 """
 body = mail_body.encode('utf-8')
 
 mailer.send_mail(body)
-
-print(body.decode('utf-8'))
