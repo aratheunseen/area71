0d77f5e0c48eb0b97d434b3e9123c3c3dc69d2aa
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-08 20:28:41 +0600

    Add API endpoints for station-wise and year-wise weather reports

--- python/02_api/weather-api/main.py
+++ python/02_api/weather-api/main.py
@@ -9,10 +9,24 @@ stations = pd.read_csv("data-small/stations.txt",skiprows=17)
 def home():
     return render_template("home.html",data=stations[["STAID","STANAME                                 "]].to_html())
 
+@app.route("/api/v1/<station>")
+def station_wise_weather_report(station):
+    filepath = "data-small/TG_STAID" + str(station).zfill(6) +".txt"
+    df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/annual/<station>/<year>")
+def year_wise_weather_report(station, year):
+    filepath = "data-small/TG_STAID" + str(station).zfill(6) +".txt"
+    df = pd.read_csv(filepath, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
 @app.route("/api/v1/<station>/<date>")
 def weather_report(station, date):
     filepath = "data-small/TG_STAID" + str(station).zfill(6) +".txt"
-    print(filepath)
     df = pd.read_csv(filepath, skiprows=20, parse_dates=['    DATE'])
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     result = {
