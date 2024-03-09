b2c54a9051f9f8c838a3e5b8a49eb741af269741
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-09 06:14:41 +0600

    Refactor weather forecast dashboard code

--- python/02_api/weather-forcast-dashboard/main.py
+++ python/02_api/weather-forcast-dashboard/main.py
@@ -1,23 +1,34 @@
 import streamlit as st
 import plotly.express as px
+from controllers import get_data
 
 st.title("Weather Forecast Dashboard")
 
 place = st.text_input("Place")
-days = st.slider("Forecast Days",min_value=0,max_value=5,
+days = st.slider("Forecast Days",min_value=1,max_value=5,
                  help="Select number for forecast days.")
-view = st.selectbox("Select data to view",("Temperatue","Sky views"))
+view = st.selectbox("Select data to view",("Temperatue","Sky condition"))
 
-st.subheader(f"{view} for the next {days} days in {place}")
+if place:
+    st.subheader(f"{view} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2024-05-01", "2024-05-02", "2024-05-03", "2024-05-02", "2024-05-03"]
-    temperatures = [1,2,3,4,5]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+    try:
+        filtered_data = get_data(place=place, forecast_days=days)
+        
 
-d, t = get_data(days)
+        if view == "Temperatue":
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            temperatures = [dict["main"]["temp"]/10 for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x": "Dates", "y": "Temperature (C)"})
+        if view == "Sky condition":
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            imagepaths = [(f'images/{status.lower()}.png') for status in sky_conditions]
 
-st.plotly_chart(figure)
+            st.image(imagepaths,width=130)
+
+    except KeyError:
+        error = f"{place} is not a valid place."
+        st.error(error)
