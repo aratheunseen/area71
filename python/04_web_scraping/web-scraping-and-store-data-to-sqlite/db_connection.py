7cdad805119427d372c99a5ddc69ed15cd920255
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-11 05:25:07 +0600

    Update database connection and perform data operations

--- python/04_web_scraping/web-scraping-and-store-data-to-sqlite/db_connection.py
+++ python/04_web_scraping/web-scraping-and-store-data-to-sqlite/db_connection.py
@@ -1,11 +1,17 @@
 import sqlite3
 
+# connection and cursor
 connection = sqlite3.connect("data.db")
 cursor = connection.cursor()
 
-
-cursor.execute("SELECT * FROM users")
-
+# read/fetch data
+cursor.execute('SELECT email FROM users WHERE id=4')
 rows = cursor.fetchall()
-
 print(rows)
+
+# insert data
+new_data = [(3,'Abdul', 'abd@ul.com', '122445'),
+            (4,'Jobdul', 'job@dul.net', '54778544')]
+
+cursor.executemany("INSERT INTO users VALUES(?,?,?,?)", new_data)
+connection.commit()
