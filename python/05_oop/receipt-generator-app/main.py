6e6a469b26878a22a877391fe4625405ef8f7710
aratheunseen <62181222+aratheunseen@users.noreply.github.com>
2024-03-13 09:14:11 +0600

    Add blank line for readability in main.py

--- python/05_oop/receipt-generator-app/main.py
+++ python/05_oop/receipt-generator-app/main.py
@@ -14,6 +14,7 @@ class Article:
         in_stock = df.loc[df['id'] == self.id, 'in stock'].squeeze()
         return in_stock
 
+
 class Receipt:
     def __init__(self, article_id, article_object):
         self.article_id = article_id
